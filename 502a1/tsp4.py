e28c3b2466b713d82b5ca7cfc9a19467ad598f8b
hudginspj <hudginspj@mymail.vcu.edu>
2018-10-10 12:59:15 -0400

    decent length now

--- 502a1/tsp4.py
+++ 502a1/tsp4.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-import random
 import math
-import itertools
+import datetime
 from exact1 import *
 
 print("hello")
@@ -17,7 +15,6 @@ def get_endpoints(points, n):
     #return min_xs[:-1], max_xs[:-1], min_ys[:-1], max_ys[:-1]
     #return min_xs, max_xs, min_ys, max_ys
     endpoints = x_sorted[:num_per_edge] + x_sorted[-num_per_edge:] + y_sorted[:num_per_edge] + y_sorted[-num_per_edge:]
-    #print(endpoints)
     return list(set(endpoints))
 
 def x_partition(cities):
@@ -30,6 +27,21 @@ def x_partition(cities):
         if p[0] < x_divider:
             part1.append(p)
         elif p[0] >= x_divider:
+            part2.append(p)
+        else:
+            raise Exception()
+    return part1, part2
+
+def y_partition(cities):
+    y_sorted = sorted(cities, key=lambda p: p[1])
+    halfway = len(cities) // 2
+    y_divider = y_sorted[halfway][1]
+    part1 = []
+    part2 = []
+    for p in cities:
+        if p[1] < y_divider:
+            part1.append(p)
+        elif p[1] >= y_divider:
             part2.append(p)
         else:
             raise Exception()
@@ -51,25 +63,21 @@ def stitch(subsol0, subsol1, endpoints0, endpoints1, top_endpoints):
                         closest_pair_0 = e0
                         closest_pair_1 = e1
 
-    print("closest pair", closest_pair_dist, closest_pair_0, closest_pair_1)
+    #print("closest pair", closest_pair_dist, closest_pair_0, closest_pair_1)
 
 
     #Start, end, distance, path
     res = []
     for s0 in subsol0:   #### TODO This makes most of the single unnecessary, unless we actually crunch all shortest paths
-        #print(s0)
         for s1 in subsol1:
             if s0[0] in top_endpoints and s1[1] in top_endpoints and s0[0] != closest_pair_0 and s1[1] != closest_pair_1:
                 if s1[0] == closest_pair_1:
-                    #print("yup")
                     if s0[1] == closest_pair_0:
-                        #print("esoji")
                         dist = s0[2] + closest_pair_dist + s1[2]
                         path = s0[3] + s1[3]
                         res.append((s0[0], s1[1], dist, path))
-    #print(res)
-    if len(res) == 0:
-        print("compensating")
+    if len(res) == 0:   #TODO how much worse does this make the solution?
+        #print("compensating")
         comps.append(len(top_endpoints))
         top_endpoints = set()
         for s0 in subsol0[:3]:
@@ -82,7 +90,6 @@ def stitch(subsol0, subsol1, endpoints0, endpoints1, top_endpoints):
                         res.append((s0[0], s1[1], dist, path))
                         res.append((s1[1], s0[0], dist, list(reversed(path))))
         top_endpoints = list(top_endpoints)
-        print("done compensating")
     else:
         non_comps.append(len(top_endpoints))
 
@@ -95,39 +102,49 @@ def stitch(subsol0, subsol1, endpoints0, endpoints1, top_endpoints):
 
 
 
-def recursive_split(cities):
-    if len(cities) < 12:
+def recursive_split(cities, depth=0):
+    
+    if len(cities) < 10:
         endpoints = get_endpoints(cities, math.sqrt(len(cities)))
         sol = best_between_endpoints_annotated(cities, endpoints) 
         return sol, endpoints
 
-    part0, part1 = x_partition(cities) 
+    if depth % 2 == 0:
+        part0, part1 = x_partition(cities)
+    else:
+        part0, part1 = y_partition(cities)
 
-    subsol0, endpoints0 = recursive_split(part0)
-    subsol1, endpoints1 = recursive_split(part1)
-    # endpoints0 = get_endpoints(part0, math.sqrt(len(part0)))
-    # subsol0 = best_between_endpoints_annotated(part0, endpoints0)
-    print(len(part0), len(endpoints0), len(subsol0))
+    subsol0, endpoints0 = recursive_split(part0, depth+1)
+    subsol1, endpoints1 = recursive_split(part1, depth+1)
+    #print(len(part0), len(endpoints0), len(subsol0))
 
-    # endpoints1 = get_endpoints(part1, math.sqrt(len(part1)))
-    # subsol1 = best_between_endpoints_annotated(part1, endpoints1)
-    print(len(part1), len(endpoints1), len(subsol1))
+    #print(len(part1), len(endpoints1), len(subsol1))
+
+    
 
     top_endpoints = get_endpoints(endpoints0+endpoints1, math.sqrt(len(cities)))
-    print("top endpoints", len(top_endpoints), top_endpoints)
+    #print("top endpoints", len(top_endpoints), top_endpoints)
+
+    if depth < 5:
+        print("  " * depth, len(cities), len(subsol0), len(subsol1), len(top_endpoints))
 
     return stitch(subsol0, subsol1, endpoints0, endpoints1, top_endpoints)
     #return sol, top_endpoints
+
+start_time = datetime.datetime.now()
+
 
 cities = gen_cities_annotated(10000,500)
 print("=" * 100)
 
 rec_sols, endpoints = recursive_split(cities)
 res_rec = best_closed_sol(rec_sols)
-print("rec", res_rec, total_distance(cities, res_rec[1]))
+print("rec", res_rec, '\n', total_distance(cities, res_rec[1]))
 
-print(comps)
-print(len(comps), len(non_comps))
+stop_time = datetime.datetime.now()
+print(stop_time - start_time)
+#print(comps)
+#print(len(comps), len(non_comps))
 
 # res_held = solve_tsp_dynamic(cities)
 # print("held", total_distance(cities, res_held[1]), res_held)
