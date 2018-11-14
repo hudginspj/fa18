832140311629ef3defcec491fb7a8abf5f698ba2
hudginspj <hudginspj@mymail.vcu.edu>
2018-11-14 13:29:10 -0500

    fixing inverions

--- 502a2/inv1.py
+++ 502a2/inv1.py
@@ -5,6 +5,7 @@ import datetime
 import time
 from heldkarp import *
 from swap1 import *
+from multi1 import *
 
 
 def ccw(a,b,c):
@@ -17,15 +18,57 @@ def is_inv(four_idx, all_cities):
 
 
 
+
 def show_inv(path, all_cities):
     for i in range(len(path)-3):
         for j in range(i+2,len(path)-1):
-            four_idx = [path[k] for k in (i, i+1, j, j+1)]
+            four_path_idx = (i, i+1, j, j+1)
+            four_idx = [path[k] for k in four_path_idx]
             if is_inv(four_idx, all_cities):
                 print(i, j)
                 plot_path(four_idx, all_cities, 'ro')
-                return
+                path = path[:i+1] + list(reversed(path[i+1:j+1])) + path[j+1:]
+                plot_path(path, all_cities, 'r:')
+                
 
+def fix_inv(path, all_cities, limit=10):
+    count = 0
+    for i in range(len(path)-3):
+        for j in range(i+2,len(path)-1):
+            four_path_idx = (i, i+1, j, j+1)
+            four_idx = [path[k] for k in four_path_idx]
+            if is_inv(four_idx, all_cities):
+                #print(i, j, j-i)
+                path = path[:i+1] + list(reversed(path[i+1:j+1])) + path[j+1:]
+                count += 1
+    print("inversions: ", count)
+    return path, count
+
+
+def inv_test3():
+    cities = gen_cities(500,500)
+
+    start_time = datetime.datetime.now()
+    path = threaded_tsp(cities)
+    plot_path(path, cities, "r:")
+    #plt.savefig("new_mp.png")
+    distance = total_distance(cities, path)
+    print("distance", distance)
+
+    path, inversions = fix_inv(path, cities)
+    path, inversions = fix_inv(path, cities)
+    path, inversions = fix_inv(path, cities)
+
+    plot_path(path, cities, "b")
+
+    distance = total_distance(cities, path)
+    print("distance", distance)
+
+    runtime = (datetime.datetime.now() - start_time).total_seconds()
+
+    plt.show()
+    
+    
 
 def inv_test():
     cities = [[0,0], [0,1], [1,0], [1,1]]
@@ -41,13 +84,16 @@ def inv_test2():
     
     distance = total_distance(cities, path)
     print(distance)
-    show_inv(path, cities)
 
     plot_path(path, cities, 'b')
+    show_inv(path, cities)
     plt.show()
 
 
+
+
+
 if __name__ == "__main__":
-    inv_test2()
+    inv_test3()
     
 
